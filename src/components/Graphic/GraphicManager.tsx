44118fa727df753db13cd4fb92b8c5f546d92951
RafaelSavoy <89671665+RafaelSavoy@users.noreply.github.com>
2022-10-05 12:29:33 -0300

    Adicionando dados para tirar print doo site

--- src/components/Graphic/GraphicManager.tsx
+++ src/components/Graphic/GraphicManager.tsx
@@ -13,7 +13,7 @@ interface GraphicInfos {
 const GraphicManager = () => {
   const [graphicInfos, setGraphicInfos] = useState<GraphicInfos>({
     graphicName: "",
-    graphicData: [],
+    graphicData: [["Item 1",50],["Item 2",50],["Item 3",50],["Item 4",50],["Item 5",50],["Item 6",50]],
     graphicStatus: false,
   });
 
