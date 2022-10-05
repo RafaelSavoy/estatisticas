72bafde25bbff863f56f14aad17ff3d9c41e1a3a
Rafael Savoy <rafaelsavoy1@outlook.com>
2022-10-05 10:58:58 -0300

    feat(header): adicionando header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyledHeader } from "./styles";
+
+const Header = () => {
+  return (
+    <StyledHeader>
+      <p>
+        Made with ❤️ by{" "}
+        <a href="https://github.com/RafaelSavoy" target="_blank">
+          Rafael Savoy
+        </a>
+      </p>
+    </StyledHeader>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/styles.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+  display: flex;
+  justify-content: center;
+  position: sticky;
+  width: 100%;
+  padding: 10px;
+  font-family: 'Poppins',sans-serif;
+  p {
+    font-size: 1.5rem;
+  }
+`;
